7a6227c431b02f6cee6e13bc1a19cc2e9c79b632
ing. Federico Fuga <fuga@studiofuga.com>
2015-06-20 12:31:50 +0200

    ledBlink example: allow serial device path to be passed from command line

--- examples/ledBlink.c
+++ examples/ledBlink.c
@@ -1,12 +1,15 @@
 
 #include "firmata.h"
 
-int             main()
+int main(int argc, char *argv[])
 {
   t_firmata     *firmata;
   int           i = 0;
 
-  firmata = firmata_new("/dev/ttyACM0"); //init Firmata
+  char *serial = "/dev/ttyACM0";
+  if (argc > 1)
+    serial = argv[1];
+  firmata = firmata_new(serial); //init Firmata
   while(!firmata->isReady) //Wait until device is up
     firmata_pull(firmata);
   firmata_pinMode(firmata, 13, MODE_OUTPUT); //set pin 13 (led on most arduino) to out
